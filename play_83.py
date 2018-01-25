f030349378638a59e2eb2bf416c15306500346bf
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2018-01-25 15:29:24 +0530

    Create play_83.py

---
+++ play_83.py
@@ -0,0 +1,12 @@
+def play_83():
+	n=int(input('Enter n:'))
+	l,max=[],-1
+	print('Enter elements :')
+	for i in range(n):
+		l.append(int(input()))
+	for i in range(0,n-1,2):
+		s=l[i]|l[i+1]
+		if max<s:
+			max=s
+	print(max)
+play_83()
