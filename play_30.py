d01eb580ef4eb412c79dc5794be589ac6472da8c
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2018-01-25 09:41:31 +0530

    Create play_30.py

---
+++ play_30.py
@@ -0,0 +1,14 @@
+def play_30():
+	c=0
+	l1=list(input('Enter string 1:'))
+	l2=list(input('Enter string 2:'))
+	for i in range(len(l1)):
+		if l1[i]!=l2[i]:
+			c+=1
+	k=int(input('Enter k :'))
+	if c==k:
+		print('Yes')
+	else :
+		print('No')
+    
+  play_30()
